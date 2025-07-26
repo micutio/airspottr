5c1c65720941981bf3a53a0e99a3b22867edb417
micutio <wagner.mchl@googlemail.com>
2025-07-26 16:15:03 +0800

    fixed warnings in main.go

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"net/http"
 	"strings"
 	"time"
@@ -26,114 +28,135 @@ const (
 	summaryInterval = 1 * time.Hour
 )
 
+var (
+	errNonOkResponse     error = errors.New("non-OK response")
+	errEmptyResponseBody error = errors.New("empty response body")
+	errNonJSONContent    error = errors.New("non-JSON content type")
+)
+
 func main() {
+	logger := slog.Default()
 	flightDash := newDashboard()
 
 	// Create a aircraftUpdateTicker that fires every 30 seconds
 	aircraftUpdateTicker := time.NewTicker(aircraftUpdateInterval)
 	defer aircraftUpdateTicker.Stop()
 
-	milAircraftUpdateTicker := time.NewTicker(milAircraftUpdateInterval)
-	defer milAircraftUpdateTicker.Stop()
 	// aircraft and military aircraft updates should not coincide to avoid exceeding the api limit.
 	// Hence, stagger them by 15 seconds.
+	milAircraftUpdateTicker := time.NewTicker(milAircraftUpdateInterval)
+	defer milAircraftUpdateTicker.Stop()
 	time.AfterFunc(milAircraftUpdateDelay, func() {
 		milAircraftUpdateTicker.Reset(milAircraftUpdateInterval)
 	})
 
-	raritySummaryTicker := time.NewTicker(summaryInterval)
-	defer raritySummaryTicker.Stop()
+	summaryTicker := time.NewTicker(summaryInterval)
+	defer summaryTicker.Stop()
 
 	// Use a channel to gracefully stop the program if needed.
 	// (Though not strictly necessary for an infinite loop)
 	done := make(chan bool)
 
-	log.Println("aircraft Tracking dashboard")
-	log.Println("Press Ctrl+C to stop the program.")
+	fmt.Println("aircraft Tracking dashboard")
+	fmt.Println("Press Ctrl+C to stop the program.")
 
 	// Start a goroutine to perform the requests
 	go func() {
 		for {
 			select {
 			case <-aircraftUpdateTicker.C:
-				requestAndProcessCivAircraft(&flightDash)
+				if err := requestAndProcessCivAircraft(&flightDash); err != nil {
+					logger.Error("main: ", slog.Any("error", err))
+				}
 			case <-milAircraftUpdateTicker.C:
-				requestAndProcessMilAircraft(&flightDash)
-			case <-raritySummaryTicker.C:
+				if err := requestAndProcessMilAircraft(&flightDash); err != nil {
+					logger.Error("main: %w", slog.Any("error", err))
+				}
+			case <-summaryTicker.C:
 				flightDash.listTypesByRarity()
 			case <-done:
 				// This case allows for graceful shutdown (not used in this example but good practice)
-				log.Println("Stopping HTTP GET request routine.")
+				logger.Info("Stopping HTTP GET request routine.")
 
 				return
 			}
 		}
 	}()
 
-	// Just for testing, remove this call later.
-	requestAndProcessMilAircraft(&flightDash)
+	// Run once in the beginning.
+	if err := requestAndProcessMilAircraft(&flightDash); err != nil {
+		logger.Error("main: ", slog.Any("error", err))
+	}
 
 	// Keep the main goroutine alive indefinitely, or until an interrupt signal is received
 	// In a real application, you might have other logic here or use a wait group.
 	select {} // Block indefinitely
 }
 
-func requestAndProcessCivAircraft(dashboard *dashboard) {
+func requestAndProcessCivAircraft(dashboard *dashboard) error {
 	// Define the URL for the HTTP GET request
 	targetURL := fmt.Sprintf(
 		"https://opendata.adsb.fi/api/v2/lat/%.6f/lon/%.6f/dist/250",
 		lat,
 		lon,
 	)
+
 	// This case is executed every time the ticker "ticks"
 	body, requestErr := sendRequest(targetURL)
 	if requestErr != nil {
-		log.Printf("Error during request: %v", requestErr)
-
-		return
+		return fmt.Errorf("requestAndProcessCivAircraft: error during request: %w", requestErr)
 	}
 
 	processingErr := dashboard.processCivAircraftJSON(body)
 
 	if processingErr != nil {
-		log.Printf("Error during processing: %v", processingErr)
+		return fmt.Errorf("requestAndProcessCivAircraft: error during processing: %w", processingErr)
 	}
+
+	return nil
 }
 
-func requestAndProcessMilAircraft(dashboard *dashboard) {
+func requestAndProcessMilAircraft(dashboard *dashboard) error {
 	// Define the URL for the HTTP GET request
 	targetURL := "https://opendata.adsb.fi/api/v2/mil"
 	// This case is executed every time the ticker "ticks"
 	body, requestErr := sendRequest(targetURL)
 	if requestErr != nil {
-		log.Printf("Error during request: %v", requestErr)
-
-		return
+		return fmt.Errorf("error during request: %w", requestErr)
 	}
 
 	processingErr := dashboard.processMilAircraftJSON(body)
 
 	if processingErr != nil {
-		log.Printf("Error during processing: %v", processingErr)
+		return fmt.Errorf("error during processing: %w", processingErr)
 	}
+
+	return nil
 }
 
 // sendRequest sends an HTTP GET request and returns a valid byte slice of the response body.
 func sendRequest(url string) ([]byte, error) {
-	resp, respErr := http.Get(url)
-	if respErr != nil {
-		return nil, fmt.Errorf("failed to send GET request: %w", respErr)
+	ctx := context.Background()
+	req, reqErr := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if reqErr != nil {
+		return nil, fmt.Errorf("sendRequest: invalid request error: %s : %w", url, reqErr)
 	}
-	defer func(bodyReader io.ReadCloser) {
-		err := bodyReader.Close()
-		if err != nil {
-			log.Printf("failed to close body reader: %v", err)
-		}
-	}(resp.Body) // Ensure the response body is closed
+
+	resp, respErr := http.DefaultClient.Do(req)
+	if respErr != nil {
+		return nil, fmt.Errorf("sendRequest: failed to send GET request: %s: %w", url, respErr)
+	}
+	defer resp.Body.Close()
+	// defer func(bodyReader io.ReadCloser) {
+	// 	err := bodyReader.Close()
+	// 	if err != nil {
+	//		slog.Default().Error("sendRequest: ", slog.Any("close body reader", err))
+	//	}
+	// }(resp.Body) // Ensure the response body is closed
 
 	// Check if the request was successful (status code 200 OK)
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("received non-OK HTTP status: %s", resp.Status)
+		return nil, fmt.Errorf("sendRequest: %w %s", errNonOkResponse, resp.Status)
 	}
 
 	// Read the response body
@@ -143,12 +166,12 @@ func sendRequest(url string) ([]byte, error) {
 	}
 
 	if len(body) == 0 {
-		return nil, fmt.Errorf("response body is empty")
+		return nil, fmt.Errorf("sendRequest: %w", errEmptyResponseBody)
 	}
 
 	contentType := resp.Header.Get("Content-Type")
 	if !strings.Contains(contentType, "application/json") {
-		return nil, fmt.Errorf("received non-JSON content-type: %s", contentType)
+		return nil, fmt.Errorf("senRequest: %w, %s", errNonJSONContent, contentType)
 	}
 
 	return body, nil
